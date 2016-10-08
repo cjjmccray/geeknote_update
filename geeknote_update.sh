fc6e86a5e1a798e5100c675d3269ab9b54795570
cjjmccray <cjjmccray@gmail.com>
2016-10-08 21:38:10 +0100

    Added noip2 log

--- geeknote_update.sh
+++ geeknote_update.sh
@@ -9,6 +9,7 @@
 ## --- ntpq
 ## --- public ip address of local router
 ## --- log file size
+## --- check on whether noip2 has updated and add logfile if it has
 ## then uploads the file to Evernote in the named NOTEBOOK
 
 MACHINEID="machine-name or hostname"
@@ -51,6 +52,18 @@ echo >> ~/geeknote/"$MACHINEID"-status
 echo geeknote_update.log file size >> ~/geeknote/"$MACHINEID"-status
 ls -l ~/geeknote_update.log >> ~/geeknote/"$MACHINEID"-status
 echo >> ~/geeknote/"$MACHINEID"-status
+
+if [ -f ~/noip2_update.log ]
+then
+   echo noip2 has updated IP address to 7bc.myvnc.com >> ~/geeknote/"$MACHINEID"-status
+   cat ~/noip2_update.log >> ~/geeknote/"$MACHINEID"-status
+   echo >> ~/geeknote/"$MACHINEID"-status
+   cat ~/noip2_update.log >> ~/noip2_update_archive.log
+   rm ~/noip2_update.log
+else
+   echo noip2 has not updated IP address to 7bc.myvnc.com >> ~/geeknote/"$MACHINEID"-status
+   echo >> ~/geeknote/"$MACHINEID"-status
+fi
 
 ## geeknote edit --title ""$MACHINEID"-status"  --notebook "Newport" --content < ~/geeknote/"$MACHINEID"-status
 gnsync --path /root/geeknote --mask "*" --logpath /root/geeknote_update.log --notebook "$NOTEBOOK"
